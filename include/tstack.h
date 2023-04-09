b71e591dd65b41fc2ea2a349e7a810d1e3d0f289
LesyaVasilek <125215125+LesyaVasilek@users.noreply.github.com>
2023-04-09 22:03:12 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,35 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+   private:
+    int v;
+    T* mass;
+    public:
+    TStack() : v(-1) {
+        mass = new T[size];
+    }
+    bool Empty() const {
+        return (v == -1);
+    }
+    bool Full() const {
+        return (v == size - 1);
+    }
+    void push(T num) {
+        if (isFull()) {
+            throw std::string("full!");
+        } else {
+            mass[++v] = num;
+        }
+    }
+    const T& get() {
+        return mass[v];
+    }
+    const T& pop() {
+        if (isEmpty()) {
+            throw std::string("empty!");
+        } else {
+            return mass[v--];
+        }
+    }
 };
-
 #endif  // INCLUDE_TSTACK_H_
