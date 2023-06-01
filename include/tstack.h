f064c8034e173b642d4ed31dd709395337f84ee4
LesyaVasilek <125215125+LesyaVasilek@users.noreply.github.com>
2023-06-01 20:40:28 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,42 +1,43 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
-#include <string>
 
-template<typename T, int size>
+template<typename T, int size1>
 class TStack {
-private:
-    int v;
-    T* mass;
+ private:
+    int size;
+    int locSize;
+    T* arr;
 
-public:
-    TStack() : v(-1) {
-        mass = new T[size];
+ public:
+    TStack() {
+      this->size = size1;
+      this->arr = new T[size];
+      this->locSize = -1;
     }
-    bool Empty() const {
-        return (v == -1);
+    ~TStack() {
+      delete []arr;
     }
-    bool Full() const {
-        return (v == size - 1);
+    void push_back(T elem) {
+      if (locSize >= size - 1) {
+        T* tmp = arr;
+        arr = new T[++size];
+        for (int i = 0; i < size; i++) arr[i] = tmp[i];
+        delete[] tmp;
+      }
+      arr[++locSize] = elem;
     }
-    void push(T num) {
-        if (Full()) {
-            throw std::string("full!");
-        }
-        else {
-            mass[++v] = num;
-        }
+    T& pop_back() {
+      size--;
+      return arr[--locSize + 1];
     }
-    const T& get() {
-        return mass[v];
+    T& getLast() {
+      return arr[locSize];
     }
-    const T& pop() {
-        if (Empty()) {
-            throw std::string("empty!");
-        }
-        else {
-            return mass[v--];
-        }
+    bool empty() {
+      if (locSize == -1) return true;
+      else
+        return false;
     }
 };
 #endif  // INCLUDE_TSTACK_H_
